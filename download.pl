f7c9ba0856d170e41e5265c255f5a52f22164192
Christopher Bowns <git@cbowns.com>
2009-12-28 13:04:26 -0500

    Remove a bunch of includes. Set up logging. Curl the url and set up parsing

--- download.pl
+++ download.pl
@@ -2,14 +2,20 @@
 
 use DBI;
 use Data::Dumper;
-use File::Basename;
-use FindBin;
 use Log::Log4perl(":easy");
-use Params::Validate;
-use Scalar::Util;
-use Sys::Hostname;
 use strict;
 use warnings;
+
+unless ( Log::Log4perl->initialized() ) {
+    my $config = '
+        log4perl.rootLogger                                 = DEBUG, Screen
+        log4perl.oneMessagePerAppender                      = 1
+        log4perl.appender.Screen                            = Log::Log4perl::Appender::Screen 
+        log4perl.appender.Screen.layout                     = Log::Log4perl::Layout::PatternLayout
+        log4perl.appender.Screen.layout.ConversionPattern   = %d %p: %m%n
+        ';
+    Log::Log4perl->init( \$config );
+}
 
 my $path = "database.sqlite3";
 
@@ -18,7 +24,25 @@ if ( !defined($db) ) {
     exit 1;
 }
 
-my $longLine = 'CREATE TABLE "song" ( 
-    "type" TEXT); ';
+# my $longLine = 'CREATE TABLE "song" (
+#     "type" TEXT); ';
+#
+# $db->do($longLine) or die "$DBI::errstr\n";
 
-$db->do($longLine) or die "$DBI::errstr\n";
+my $popularFile = "feed.popular.xml";
+my $popularUrl  = "http://hypem.com/feed/popular/lastweek/1/feed.xml";
+
+if ( !-e $popularFile ) {
+    DEBUG("curling the url: $popularUrl");
+
+    # `curl http://cbowns.com/ -o $popularFile`;
+    `curl -A "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)" $popularUrl -o feed.popular.xml`;
+}
+else {
+    DEBUG("file $popularFile already exists");
+}
+
+use XML::Parser;
+
+my $p1 = new XML::Parser( Style => 'Tree' );
+$p1->parsefile($popularFile);
