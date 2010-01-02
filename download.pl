71e5c5b83b93aec33d44dda42e91aaaabaf814fc
Christopher Bowns <git@cbowns.com>
2010-01-02 18:50:37 -0500

    Re-indent for perltidy: `perltidy -et=4 --tabs`

--- download.pl
+++ download.pl
@@ -7,14 +7,14 @@ use strict;
 use warnings;
 
 unless ( Log::Log4perl->initialized() ) {
-    my $config = '
-        log4perl.rootLogger                                 = DEBUG, Screen
-        log4perl.oneMessagePerAppender                      = 1
-        log4perl.appender.Screen                            = Log::Log4perl::Appender::Screen 
-        log4perl.appender.Screen.layout                     = Log::Log4perl::Layout::PatternLayout
-        log4perl.appender.Screen.layout.ConversionPattern   = %d %p: %m%n
-        ';
-    Log::Log4perl->init( \$config );
+	my $config = '
+	log4perl.rootLogger                                 = DEBUG, Screen
+	log4perl.oneMessagePerAppender                      = 1
+	log4perl.appender.Screen                            = Log::Log4perl::Appender::Screen 
+	log4perl.appender.Screen.layout                     = Log::Log4perl::Layout::PatternLayout
+	log4perl.appender.Screen.layout.ConversionPattern   = %d %p: %m%n
+	';
+	Log::Log4perl->init( \$config );
 }
 
 # =================================
@@ -27,9 +27,9 @@ my $db = DBI->connect( "dbi:SQLite:$path", "", "" );
 exit 1 if !defined($db);
 
 if ($createTable) {
-    my $longLine = 'CREATE TABLE "song" (
-        "type" TEXT); ';
-    $db->do($longLine) or die "$DBI::errstr\n";
+	my $longLine = 'CREATE TABLE "song" (
+		"type" TEXT); ';
+	$db->do($longLine) or die "$DBI::errstr\n";
 }
 
 # ==============================
@@ -39,13 +39,13 @@ my $popularFile = "feed.popular.xml";
 my $popularUrl  = "http://hypem.com/feed/popular/lastweek/1/feed.xml";
 
 if ( !-e $popularFile ) {
-    DEBUG("curling the url: $popularUrl");
+	DEBUG("curling the url: $popularUrl");
 
-    # `curl http://cbowns.com/ -o $popularFile`;
-    `curl -A "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)" $popularUrl -o feed.popular.xml`;
+	# `curl http://cbowns.com/ -o $popularFile`;
+`curl -A "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)" $popularUrl -o feed.popular.xml`;
 }
 else {
-    DEBUG("file $popularFile already exists");
+	DEBUG("file $popularFile already exists, using filesystem cache");
 }
 
 # ===================
