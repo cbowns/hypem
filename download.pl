b9502c7011201d2a48c3ef50b3e4356d74e9ba88
Christopher Bowns <git@cbowns.com>
2010-01-02 18:50:59 -0500

    Validate params. Fix up logger. Do a search down the tree for our desired values. These keys are subject to change in the future, I'm sure. When we find the value we want, the subroutine returns [val+1] so that we have the next subarray to keep searching in.

--- download.pl
+++ download.pl
@@ -5,6 +5,7 @@ use Data::Dumper;
 use Log::Log4perl(":easy");
 use strict;
 use warnings;
+use Params::Validate;
 
 unless ( Log::Log4perl->initialized() ) {
 	my $config = '
@@ -16,6 +17,8 @@ unless ( Log::Log4perl->initialized() ) {
 	';
 	Log::Log4perl->init( \$config );
 }
+
+my $logger = Log::Log4perl::get_logger();
 
 # =================================
 # = database stuff: create the db =
@@ -55,17 +58,50 @@ else {
 use XML::Parser;
 
 my $p1 = new XML::Parser( Style => 'Tree' );
-DEBUG( Dumper( $p1->parsefile($popularFile) ) );
+my $popularTree = $p1->parsefile($popularFile);
+
+my @search = ( 'rss', 'channel', 'item', 'link', 'http://hypem.com' );
+my $currentLevel = $popularTree;
+foreach my $currentSearch (@search) {
+	$currentLevel =
+	  findUntil( $currentLevel, $currentSearch,
+		( $currentSearch =~ "http" ? 1 : 0 ) );
+}
+$logger->debug( "Currently found: " . Dumper($currentLevel) );
+
+=head2 findUntil
+
+Finds the array item that matches searchString (I'm doing a =~ with it), and returns $arrayRef->[position + 1].
 
 
+Usage:
 
+	my $foundVal = findUntil($arrayRef, "searchString");
 
+If you want $arrayRef->[position], pass a 1 for literalMatch:
 
+	my $foundVal = findUntil($arrayRef, "searchString", 1);
 
+=cut
 
+sub findUntil {
+	my ( $itemRef, $searchString, $literalMatch ) =
+	  validate_pos( @_, 1, 1, { default => 0 } );
+	$logger->debug("looking for $searchString, literalMatch is $literalMatch");
 
-
-
+	my $count = 0;
+	foreach my $test ( @{$itemRef} ) {
+		if ( defined( $itemRef->[$count] ) ) {
+			return $itemRef->[ $count + ( $literalMatch ? 0 : 1 ) ]
+			  if ( $itemRef->[$count] =~ $searchString );
+			$count++;
+		}
+		else {
+			$logger->error("never found $searchString, returning null");
+			return;
+		}
+	}
+}
 
 # twitter will be harder.
 # curl -A "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)"
